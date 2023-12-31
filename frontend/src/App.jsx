c96e07455564633b8f49fc18d4e1e291d1170ba6
SyedAbdulrab <syedabdulrab1133@gmail.com>
2023-12-31 23:47:37 +0500

    add route paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,11 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
+      <Route path='/' element={}/>
+      <Route path='/home' element={}/>
+      <Route path='/hoe' element={}/>
+      <Route path='/books' element={}/>
+      <Route path='/books-2' element={}/>
     </Routes>
       <div className='bg-red-400 text-white'>App</div>
     </>
