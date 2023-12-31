a295e8ebdfdfddda57f03b9c99d5f6272d14bd27
SyedAbdulrab <syedabdulrab1133@gmail.com>
2024-01-01 00:28:06 +0500

    Man I HATE React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,24 @@
-import './App.css'
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import CreateBooks from "./pages/CreateBooks";
+import DeleteBooks from "./pages/DeleteBooks";
+import EditBook from "./pages/EditBook";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path='/' element={}/>
-      <Route path='/home' element={}/>
-      <Route path='/hoe' element={}/>
-      <Route path='/books' element={}/>
-      <Route path='/books-2' element={}/>
-    </Routes>
-      <div className='bg-red-400 text-white'>App</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books/create" element={<CreateBooks />} />
+        <Route path="/books/details/:id" element={<ShowBook />} />
+        <Route path="/books/edit/:id" element={<EditBook />} />
+        <Route path="/books/delete/:id" element={<DeleteBooks />} />
+      </Routes>
+      <div className="bg-red-400 text-white">App</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
